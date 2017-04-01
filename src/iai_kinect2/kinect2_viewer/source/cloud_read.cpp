000e5cbd662daa1ba5c3dc21e36dfab59a27bec8
jackros1022 <1347755930@qq.com>
2017-04-01 19:56:08 +0800

    修改

---
+++ src/iai_kinect2/kinect2_viewer/source/cloud_read.cpp
@@ -0,0 +1,9 @@
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+  // Set up ROS.
+  ros::init(argc, argv, "cloud_read");
+
+  ROS_INFO("Hello world!");
+}
---
+++ src/iai_kinect2/kinect2_viewer/source/cylinder_segmentation.cpp
@@ -0,0 +1,9 @@
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+  // Set up ROS.
+  ros::init(argc, argv, "cylinder_segmentation");
+
+  ROS_INFO("Hello world!");
+}

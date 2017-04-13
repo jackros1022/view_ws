e319e34b75e1d89184c1a7105caec1f7a0be92a4
jackros1022 <1347755930@qq.com>
2017-04-13 12:53:45 +0800

    4.13

---
+++ src/iai_kinect2/kinect2_viewer/6DOF/viewer_6dof.cpp
@@ -0,0 +1,9 @@
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "viewer_6DOF");
+  ros::NodeHandle nh;
+
+  ROS_INFO("Hello world!");
+}
---
+++ src/iai_kinect2/kinect2_viewer/6DOF/viewer_6dof2.cpp
@@ -0,0 +1,9 @@
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "view_6dof2");
+  ros::NodeHandle nh;
+
+  ROS_INFO("Hello world!");
+}
